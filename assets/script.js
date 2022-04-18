1bb773da1e6bc6eb433d56b55966394bb842db1d
Justin Stokes <zaire.DEV@gmail.com>
2022-04-17 21:15:06 -0400

    Finishing challenges

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,6 @@ let action = prompt('Would you like to Deposit Money or Withdraw Money?');
 if (action === 'deposit' || action === 'Deposit') {
   let deposit = prompt('How Much Would You Like To Deposit?'); 
   alert(`Your New Balance is: ${balance + deposit}`)
-  var balance = balance + deposit;
-  console.log(`${balance}`) 
   } else {
     let withdraw = prompt('How Much Would You Like To Withdraw?')
     alert(`Your New Balance is: ${balance - withdraw}`)  
